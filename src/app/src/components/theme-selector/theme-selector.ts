9ce75be872c7e2ffa64526dbaf4a05480c3d4d38
Marvin Brouwer <marvin-brouwer@live.nl>
2023-01-31 20:39:25 +0100

    [OTHER] Added hybrid solid component for the theme button

--- src/app/src/components/theme-selector/theme-selector.ts
+++
@@ -1,8 +0,0 @@
-
-const htmlNode = document.body.parentElement;
-const themeSelector = document.getElementById<HtmlSelectElement>('theme');
-
-themeSelector.addEventListener('change', () => {
-    htmlNode.setAttribute('data-theme', themeSelector.value);
-    // TODO set cookie
-})
---
+++ src/app/src/components/theme-selector/theme-selector.tsx
@@ -0,0 +1,81 @@
+import "../die/die.css";
+import "./theme-selector.css";
+
+import { Component, onMount, onCleanup } from "solid-js";
+import { createSignal, createEffect } from "solid-js";
+import Cookie from "js-cookie";
+
+interface Props {
+    initialTheme: 'auto' | 'dark' | 'light'
+}
+
+export const ThemeSelector: Component<Props> = ({ initialTheme }) => {
+
+    function setPreferredTheme(theme: 'auto' | 'light' | 'dark') {
+
+        const htmlNode = document.body.parentElement;
+        htmlNode?.setAttribute('data-theme', theme);
+
+        setSelectedTheme(theme);
+        Cookie.set('theme', theme, { expires: 100 })
+    }
+
+    const [selectedTheme, setSelectedTheme] = createSignal(initialTheme);
+    const [preferredColorScheme, setPreferredColorScheme] = createSignal(initialTheme);
+        
+    const handleColorSchemeChange = (event: MediaQueryListEvent) => {
+        setPreferredColorScheme(event.matches ? 'dark' : 'light');
+    }
+
+    function isSelected(theme: 'auto' | 'light' | 'dark'): boolean {
+
+        return selectedTheme() == theme;
+    }
+
+    function isActive(theme: 'auto' | 'light' | 'dark'): boolean {
+        if (selectedTheme() !== 'auto') return isSelected(theme);
+
+        return preferredColorScheme() == theme;
+    }
+    const [autoSelected, setAutoSelected] = createSignal(isSelected('auto'));
+    const [lightSelected, setLightSelected] = createSignal(isSelected('light'));
+    const [darkSelected, setDarkSelected] = createSignal(isSelected('dark'));
+    const [lightActive, setLightActive] = createSignal(isActive('light'));
+    const [darkActive, setDarkActive] = createSignal(isActive('dark'));
+
+    createEffect(() => {
+        setAutoSelected(isSelected('auto'));
+        setLightSelected(isSelected('light'));
+        setDarkSelected(isSelected('dark'));
+        setLightActive(isActive('light'));
+        setDarkActive(isActive('dark'));
+        
+    }, [selectedTheme, preferredColorScheme]);
+
+    onMount(() => {
+        const colorSchemeMount = window.matchMedia('(prefers-color-scheme: dark)');
+        setPreferredColorScheme(colorSchemeMount.matches ? 'dark' : 'light');
+        colorSchemeMount
+            .addEventListener("change", handleColorSchemeChange)
+    });
+  
+    onCleanup(() => {
+        window.matchMedia('(prefers-color-scheme: dark)')
+            .removeEventListener("change", handleColorSchemeChange)
+    })
+
+    return (
+        <span class="theme-selector">
+            <span role="img" class="theme-icon die">
+                <span aria-hidden={!autoSelected()} class="small-icon">⚙️</span>
+                <span aria-hidden={!lightActive()}>🌞</span>
+                <span aria-hidden={!darkActive()}>🌒</span>
+            </span>
+            <select id="theme" onChange={(e) => setPreferredTheme(e.currentTarget.value as any)}>
+                <option value="auto" selected={autoSelected()}>System preference</option>
+                <option value="light" selected={lightSelected()}>Light theme</option>
+                <option value="dark" selected={darkSelected()}>Dark theme</option>
+            </select>
+        </span>
+    );
+}
