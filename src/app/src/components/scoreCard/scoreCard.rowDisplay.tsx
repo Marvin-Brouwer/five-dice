2b96d8b33988da3d1e649d1585200b4f5d78a4d6
Marvin Brouwer <marvin-brouwer@live.nl>
2023-02-06 23:29:23 +0100

    [OTHER] Implemented calculateScore

--- src/app/src/components/scoreCard/scoreCard.rowDisplay.tsx
+++ src/app/src/components/scoreCard/scoreCard.rowDisplay.tsx
@@ -1,20 +1,50 @@
 import "./scoreCard.css";
 
 import type { Accessor, Component, JSX } from "solid-js";
+import type { ScoreField } from '../../game/gameConstants';
+import { isDiscarded } from '../../game/score/score';
+import { calculateScore } from '../../game/score/scoreCalculator';
+import type { ScorePadAccessor } from "../../game/score/useScorePad";
 
 type Props = {
-    displayLabel: Accessor<string | undefined | JSX.Element>,
-    displayRoll: Accessor<string | undefined | JSX.Element>,
-    displayScore: Accessor<string | undefined | JSX.Element>,
+    icon?: JSX.Element;
+    field: ScoreField,
+    scorePad: ScorePadAccessor,
+    displayRoll: Accessor<string | undefined | JSX.Element>
 }
 
-export const RowDisplay : Component<Props> = ({ displayLabel, displayRoll, displayScore }) => {
+
+const labels: Record<ScoreField, string> = {
+    'aces': "Aces",
+    'deuces': "Deuces",
+    'threes': "Threes",
+    'fours': "Fours",
+    'fives': "Fives",
+    'sixes': "Sixes",
+
+    'threeOfKind' : "Three of a kind",
+    'fourOfKind' : "Four of a kind",
+    'fullHouse' : "Full house",
+    'smallStraight' : "Small straight",
+    'largeStraight' : "Large straight",
+    'flush' : "Flush",
+    'chance' : "Chance",
+};
+
+export const RowDisplay : Component<Props> = ({ icon, field, scorePad, displayRoll }) => {
+
+    const displayScore = () => {
+        const fieldScore = scorePad()[field];
+        if (fieldScore === undefined) return ".";
+        if (isDiscarded(fieldScore)) return "/";
+
+        return calculateScore(scorePad(), field);
+    }
 
     return (
-
         <tr>
             <td>
-                {displayLabel}
+                {icon}{labels[field]}
             </td>
             <td>
                 {displayRoll}
