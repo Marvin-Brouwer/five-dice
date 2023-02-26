978a5b0ee7885b1d490d18c8eb3ef0137f8b2853
Marvin Brouwer <marvin-brouwer@live.nl>
2023-02-26 15:35:24 +0100

    [OTHER] Added display for flush discard selection

--- src/app/src/components/game/game.tsx
+++ src/app/src/components/game/game.tsx
@@ -1,72 +1,11 @@
 import { createSignal, Component, onMount, createEffect } from 'solid-js';
 import { useScorePad } from '../../game/score/useScorePad';
-import { score, discard } from '../../game/score/score';
 import { ScoreCard } from '../scoreCard/scoreCard';
 import { ScoreInputDialog } from '../scoreInput/scoreInput';
 
 const round = createSignal(1);
 const scorePad = useScorePad();
-const [getScorePad, applyScore] = scorePad;
-
-// TODO check scores with outliers (3 of a kind with 4 or 5 values)
-// applyScore({
-//     field: 'aces',
-//     score: score([1,2,4,1,4])
-// })
-// setRound((previous) => previous +1);
-applyScore({
-    field: 'threes',
-    score: discard()
-})
-// setRound((previous) => previous +1);
-applyScore({
-    field: 'threeOfKind',
-    score: score([6,4,4,1,4])
-})
-// setRound((previous) => previous +1);
-// applyScore({
-//     field: 'fourOfKind',
-//     score: score([5,5,5,5,3])
-// })
-// setRound((previous) => previous +1);
-// applyScore({
-//     field: 'fullHouse',
-//     score: score([5,5,5,3,3])
-// })
-// setRound((previous) => previous +1);
-// applyScore({
-//     field: 'smallStraight',
-//     score: score([1,2,3,4,2])
-// })
-// setRound((previous) => previous +1);
-// applyScore({
-//     field: 'largeStraight',
-//     score: score([6,5,4,3,2])
-// }) 
-// setRound((previous) => previous +1);
-// applyScore({
-//     field: 'flush',
-//     score: score([2,2,2,2,2]), 
-//     discard: 'deuces'
-// })
-// setRound((previous) => previous +1);
-// applyScore({
-//     field: 'flush',
-//     score: score([2,2,2,2,2]),
-//     discard: 'fours'
-// })
-// setRound((previous) => previous +1);
-// applyScore({
-//     field: 'flush',
-//     score: score([3,3,3,3,3]),
-//     discard: 'fives'
-// })
-// setRound((previous) => previous +1);
-// applyScore({
-//     field: 'chance',
-//     score: score([3,3,4,3,3])
-// })
-// setRound((previous) => previous +1);
+const [getScorePad] = scorePad;
 
 const createPlayerNameSignal = () => {
     
