3d9b933da3f6b2b5081a47708840359f4f56908e
Marvin Brouwer <marvin-brouwer@live.nl>
2023-02-26 14:42:55 +0100

    [OTHER] Created name field

--- src/app/src/components/scoreCard/scoreCard.tsx
+++ src/app/src/components/scoreCard/scoreCard.tsx
@@ -1,4 +1,5 @@
 import "./scoreCard.css";
+import EraserIcon from "../../../public/iconmonstr-eraser-1.svg?raw";
 
 import type { ScorePadAccessor } from '../../game/score/useScorePad';
 
@@ -6,20 +7,38 @@ import type { Component, Signal } from "solid-js";
 import { PartOne } from './scoreCard.partOne';
 import { PartTwo } from './scoreCard.partTwo';
 import { Totals } from './scoreCard.totals';
-import type { Accessor } from 'solid-js';
+import { createSignal, createEffect, onMount } from 'solid-js';
 
 interface Props {
     round: Signal<number>,
-    playerName: Accessor<string>,
+    playerName: Signal<string>,
     getScorePad: ScorePadAccessor
 }
 
-export const ScoreCard: Component<Props> = ({ playerName, round: [getRound], getScorePad: scorePad }) => {
+export const ScoreCard: Component<Props> = ({ 
+    playerName: [getPlayerName, setPlayerName], round: [getRound], getScorePad: scorePad 
+}) => {
+
+    const [getNameInputRef, setNameInputRef] = createSignal<HTMLInputElement>();
+
+    const [getNameDisabled, setNameDisabled] = createSignal(true);
+    onMount(() => setNameDisabled(false));
 
     return (
         <section id="score-card" role="document">
             <aside role="banner">
-                <span class="name">{playerName}</span>
+                <span class="name">
+                    <input ref={setNameInputRef}
+                        disabled={getNameDisabled()} type="text" class="name-input" 
+                        placeholder={getNameDisabled() ? "..." : "Your name here" }
+                        value={getPlayerName()} onInput={e => setPlayerName(e.currentTarget.value)} />
+                    <button 
+                        class="clear-name" aria-label="Clear the name value" innerHTML={EraserIcon} 
+                        onClick={() => {
+                            setPlayerName("");
+                            getNameInputRef()?.focus();
+                        }} />
+                </span>
                 <span class="round-number">
                     <span class="round-label">Round</span>
                     {getRound}
