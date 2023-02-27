bba321ffb5f19201bfee8a5001e803bca1879f16
Marvin Brouwer <marvin-brouwer@live.nl>
2023-02-28 00:15:43 +0100

    [OTHER] Embedding audio files

--- src/app/astro.config.ts
+++ src/app/astro.config.ts
@@ -1,7 +1,34 @@
 import { defineConfig, getViteConfig } from 'astro/config';
+import fs from 'fs';
 
 // https://astro.build/config
 import solidJs from "@astrojs/solid-js";
+
+/**
+ * Normally you would never do this.
+ * This just increases your bundle size.
+ * However, GitHub pages doesn't allow for audio hosting
+ */
+/** @type {import('vite').Plugin} */
+const blobLoader = (extension: string, mimeType:string) => ({
+    name: 'hex-loader',
+    async transform(code: any, id: string) {
+        const [filePath, query] = id.split('?');
+        if (query !== 'blob') return null
+        if (!filePath.endsWith(extension))
+            return null;
+
+        const buffer = await fs.promises.readFile(filePath);
+        const byteArray = new Uint8Array(buffer.byteLength);
+        buffer.copy(byteArray);
+
+        return `
+            const data = new Uint8Array([${byteArray}]);
+            const blobData = new Blob([data], { type: '${mimeType}' });
+            export default blobData
+        `;
+    }
+});
 
 // https://astro.build/config
 export default defineConfig({
@@ -9,12 +36,13 @@ export default defineConfig({
     base: '/five-dice/',
     output: "static",
     integrations: [solidJs()],
-    vite: getViteConfig({
+    vite: {
+        plugins: [blobLoader('.wav', 'audio/wav'), blobLoader('.mp3', 'audio/mp3')],
         optimizeDeps: {
             exclude: ['astro']
         },
         build: {
             target: 'esnext'
         }
-    })
+    }
 });
