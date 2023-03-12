f4ba1467b436fa663a8c979fadbd4e894afca3d5
Marvin Brouwer <marvin-brouwer@live.nl>
2023-03-12 15:36:13 +0100

    [OTHER] Fixed keyboard glitch

--- src/app/src/components/scoreInput/scoreInput.scoreButtons.tsx
+++ src/app/src/components/scoreInput/scoreInput.scoreButtons.tsx
@@ -4,6 +4,7 @@ import { Accessor, Component, createReaction, Setter, onMount } from 'solid-js';
 import type { ScoreInputState } from "./scoreInput.state"
 import { DieInput } from '../die/input/die-input';
 import { createMemo } from 'solid-js';
+import { createKeyboardState, DieInputKeyboard } from '../die/input/die-input.keyboard';
 
 type Props = {
     inputState: ScoreInputState,
@@ -17,6 +18,8 @@ export const ScoreInputButtons: Component<Props> = ({
     inputState, setFirstDiceRef, getSubmitButtonRef, scoreSet
 }) => {
 
+    const keyboardState = createKeyboardState();
+    
     const fieldsDisabled = createMemo(() => ([
         () => false,
         () => inputState.diceSelector.getScoreForDie(0) === undefined,
@@ -57,21 +60,25 @@ export const ScoreInputButtons: Component<Props> = ({
 
     // TODO make shift+tab and tab loop around
 
-    return <div class="score-input-buttons">
-        <DieInput modal={true} ref={(el) => { setFirstDiceRef(el); refs[0] = el; }} name='die-1' 
-            disabled={fieldsDisabled()[0]} value={inputState.diceSelector.getSignalForDie(0)}
-            autoFocus={inputState.autoFocus} />
-        <DieInput modal={true} ref={refs[1]!} name='die-2' 
-            disabled={fieldsDisabled()[1]} value={inputState.diceSelector.getSignalForDie(1)} 
-            autoFocus={inputState.autoFocus}/>
-        <DieInput modal={true} ref={refs[2]!} name='die-3' 
-            disabled={fieldsDisabled()[2]} value={inputState.diceSelector.getSignalForDie(2)} 
-            autoFocus={inputState.autoFocus}/>
-        <DieInput modal={true} ref={refs[3]!} name='die-4' 
-            disabled={fieldsDisabled()[3]} value={inputState.diceSelector.getSignalForDie(3)} 
-            autoFocus={inputState.autoFocus}/>
-        <DieInput modal={true} ref={refs[4]!} name='die-5' 
-            disabled={fieldsDisabled()[4]} value={inputState.diceSelector.getSignalForDie(4)} 
-            autoFocus={inputState.autoFocus}/>
-    </div>
+    return <>
+        <DieInputKeyboard 
+            keyboardState={keyboardState} />
+        <div class="score-input-buttons">
+            <DieInput keyboard={keyboardState.accessor} ref={(el) => { setFirstDiceRef(el); refs[0] = el; }} name='die-1' 
+                disabled={fieldsDisabled()[0]} value={inputState.diceSelector.getSignalForDie(0)}
+                autoFocus={inputState.autoFocus} />
+            <DieInput keyboard={keyboardState.accessor} ref={refs[1]!} name='die-2' 
+                disabled={fieldsDisabled()[1]} value={inputState.diceSelector.getSignalForDie(1)} 
+                autoFocus={inputState.autoFocus}/>
+            <DieInput keyboard={keyboardState.accessor} ref={refs[2]!} name='die-3' 
+                disabled={fieldsDisabled()[2]} value={inputState.diceSelector.getSignalForDie(2)} 
+                autoFocus={inputState.autoFocus}/>
+            <DieInput keyboard={keyboardState.accessor} ref={refs[3]!} name='die-4' 
+                disabled={fieldsDisabled()[3]} value={inputState.diceSelector.getSignalForDie(3)} 
+                autoFocus={inputState.autoFocus}/>
+            <DieInput keyboard={keyboardState.accessor} ref={refs[4]!} name='die-5' 
+                disabled={fieldsDisabled()[4]} value={inputState.diceSelector.getSignalForDie(4)} 
+                autoFocus={inputState.autoFocus}/>
+        </div>
+    </>
 }
